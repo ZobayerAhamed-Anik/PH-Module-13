ed7b4254a612ed3f346150a62ab4a39087e30bf8
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-12 01:03:39 +0600

    Problem-3: Factorial of a Number...

---
+++ Module-13.2/problem-03.js
@@ -0,0 +1,14 @@
+//Problem-3: Factorial of a Number...
+
+function factorial(number){
+    if(number === 0){
+        return 1;
+    }
+    let fact = 1;
+    for(let i = number; i > 0; i--){
+    fact *= i;
+    }
+    return fact;
+}
+
+console.log(factorial(7));

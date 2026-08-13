f688e25ac073baf09181523391302673423cd12e
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-14 02:43:34 +0600

    Fixing the looping of FizzBuzz form number 1 to the given number

--- Module-13.2/problem-04.js
+++ Module-13.2/problem-04.js
@@ -1,20 +1,21 @@
 //Problem-4: FizzBuzz (1 to n)
 // Loop through numbers from 1 to n. If a number is divisible by 3, print "Fizz"; if divisible by 5, print "Buzz"; and if divisible by both, print "FizzBuzz"; otherwise, print the number itself.
 
-function checkFizzBuzz(number){
-    if(number % 3 === 0 && number % 5 === 0){
-        return "FizzBuzz";
+function checkFizzBuzz(upToNumber){
+    let fizzBuzz;
+    for(let i = 1; i <= upToNumber; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            fizzBuzz = "FizzBuzz";
+        }
+        else if(i % 3 === 0){
+            fizzBuzz = "Fizz";
+        }
+        else if(i % 5 === 0){
+            fizzBuzz = "Buzz";
+        }
+        else fizzBuzz = i;
+        console.log(i, " => ",fizzBuzz);
     }
-    else if(number % 3 === 0){
-        return "Fizz";
-    }
-    else if(number % 5 === 0){
-        return "Buzz";
-    }
-    else return number;
 }
 
-console.log(checkFizzBuzz(12));
-console.log(checkFizzBuzz(10));
 console.log(checkFizzBuzz(15));
-console.log(checkFizzBuzz(77));

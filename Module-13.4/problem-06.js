9333165c0a6d142c385c5b93bec946764172a7b2
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-14 15:41:36 +0600

    Count vowel in a String

--- Module-13.4/problem-06.js
+++ Module-13.4/problem-06.js
@@ -1,1 +1,18 @@
-//Problem 6: Count Vowel in a String. vowel =['a', 'e', 'i', 'o', 'u']
+//Problem 6: Count Vowel in a String. vowel
+let myString = "programming hero";
+function countVowels(myString){
+    let vowels = "aeiou";
+    let count = 0;
+
+    for(let i = 0; i < myString.length; i++){
+        letter = myString[i];
+
+        if (vowels.includes(letter)){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(myString + " has " + countVowels(myString) + " vowels.");
+console.log("bangladesh has " + countVowels("bangladesh") + " vowels.");
